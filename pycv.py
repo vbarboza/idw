65a10d1fe6025d82fdc7643d6839dee9e481504f
Vinicius Barboza <viniciusdearaujob@gmail.com>
2017-12-09 12:31:35 -0200

    Initial commit.

---
+++ pycv.py
@@ -0,0 +1,22 @@
+# coding=utf-8
+
+# Developed and tested with:
+#
+# os:                 Windows 10 Pro
+# environment:        miniconda3
+# jupyter version:    4.3.0
+# python version:     3.6.2
+# opencv version:     3.3.0
+# matplotlib version: 2.0.2
+# numpy version:      1.13.1
+
+# Notes:
+#
+#
+
+# Use:
+#
+#    python pycv.py img/[filename]
+
+if __name__=="__main__":
+	print("Hello, world!")
